fdcdfcd1aacadc5932c581338adc557b0f862cf3
ajay-creator <ajayveeraveni119@gail.com>
2022-02-04 00:58:15 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// API
+import { fetchData, fectchDailyData } from './api';
+
+// components
+import { Cards } from './components/Cards';
+import { Charts } from './components/Charts';
+import { StatePicker } from './components/StatePicker';
+
+class App extends React.Component{
+  state = {
+    data:{},
+    dailyData:[],
+    stateName:'',
+  }
+
+  
+  async componentDidMount(){
+    const fetchedData = await fetchData('TT');
+    const fetchedDailyData = await fectchDailyData('TT');
+    this.setState({data:fetchedData, dailyData:fetchedDailyData});
+  }
+
+  handleStateChange = async (stateName) => {
+    const fetchedData = await fetchData(stateName);
+    const fetchedDailyData = await fectchDailyData(stateName);
+    this.setState({data: fetchedData, dailyData: fetchedDailyData, stateName: stateName});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Covid-19 Tracker</h1>
+        <Cards data={this.state.data}/>
+        <StatePicker handleStateChange={this.handleStateChange} />
+        <Charts dailyData={this.state.dailyData}/>
+        <p>Data is being fetched from <a href='https://covid19tracker.in/'>COVID19TRACKER.IN</a></p>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
